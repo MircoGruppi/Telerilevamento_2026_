3e574804b97ba0bbd4b1f613b419af2821c29dde
MircoGruppi <mircogruppi.gruppi@gmail.com>
2026-04-23 15:45:29 +0200

    Create 08_functions.R

---
+++ Code/08_functions.R
@@ -0,0 +1,13 @@
+#Le mie funzioni
+
+somma <- function(x,y){
+    z=x+y #definisce funzione
+    return(z) #restituisce risultato
+    }
+
+#funzione chiamata differenza
+
+diff <- function(x,y){
+    z=x-y #definisce funzione
+    return(z) #restituisce risultato
+    }
