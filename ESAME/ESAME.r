bfb2aecf151a61483e7c4fe19fdabf03b99d1b4b
MircoGruppi <mircogruppi.gruppi@gmail.com>
2026-09-03 22:50:56 +0200

    Update ESAME.r

--- ESAME/ESAME.r
+++ ESAME/ESAME.r
@@ -1,9 +1,10 @@
 #Impostazione della working directory
-setwd("~/Desktop/Università/Telerilevamento") 
-getwd() #verifica della working directory
-list.files() #lista dei file all'interno della working directory
 
-### Caricamento pacchetti
+setwd("~/Desktop/Università/Telerilevamento") 
+getwd()       # Verifica della working directory
+list.files()  # Lista dei file all'interno della working directory
+
+# Caricamento pacchetti
 library(terra)     # Per la gestione di dati raster. 
 library(imageRy)   # Gestione, analisi e visualizzazione multiframe di immagini raster
 library(ggplot2)   # Creazione di grafici personalizzati
@@ -14,24 +15,25 @@ library(ggridges)  # Grafici a cresta per visualizzare distribuzioni continue
 # Funzione per tagliare raster usando un vettore
 
 clipCoast <- function(img, boundary){
-  img_crop <- crop(img, boundary)
-  img_mask <- mask(img_crop, boundary)
+  img_crop <- crop(img, boundary)       # Ritaglia il raster all'estensione dello shapefile
+  img_mask <- mask(img_crop, boundary)  # Mantiene i pixel interni al perimetro dello shapefile
   return(img_mask)
   }
 
-#Importa Raster
+# Importazione dati raster da Sentinel-2
 
-pre2018 <- rast("Australia_2018_bands.tif")
+pre2018 <- rast("Australia_2018_bands.tif") # rast() permette di importare SpatRaster
 post2020 <- rast("Australia_2020_bands.tif")
 post2026 <- rast("Australia_2026_bands.tif")
-pre2018
+pre2018                                     # Mostra bande -> Blu=B2, Verde=B3, Rosso=B4, NIR=B8
 post2020
 post2026
 
-#mostra layer separati
-im.multiframe(3,4)
+# Mostra layer separati
 
-plot(pre2018[[1]], col = rocket(100), main = "2018, B2")
+im.multiframe(3,4) #suddivisione della finestra grafica in 3 righe e 4 colonne
+
+plot(pre2018[[1]], col = rocket(100), main = "2018, B2") #Mostra valori della prima banda
 plot(pre2018[[2]], col = rocket(100), main = "2018, B3")
 plot(pre2018[[3]], col = rocket(100), main = "2018, B4")
 plot(pre2018[[4]], col = rocket(100), main = "2018, B8")
@@ -46,36 +48,41 @@ plot(post2026[[2]], col = rocket(100), main = "2026, B3")
 plot(post2026[[3]], col = rocket(100), main = "2026, B4")
 plot(post2026[[4]], col = rocket(100), main = "2026, B8")
 
-#visualizzazione a colori naturali
+#Visualizzazione a colori naturali
+
 im.multiframe(1,3)
 
-plotRGB(pre2018, r=3, g=2, b=1, stretch = "lin", main = "2018") #pacchetto Terra
-plotRGB(post2020, r=3, g=2, b=1, stretch = "lin", main = "2020")
-plotRGB(post2026, r=3, g=2, b=1, stretch = "lin", main = "2026")
+plotRGB(pre2018, r=3, g=2, b=1, stretch = "lin", main = "2018")  # pacchetto Terra
+plotRGB(post2020, r=3, g=2, b=1, stretch = "lin", main = "2020") # Allarga i valori centrali per aumentare contrasto
+plotRGB(post2026, r=3, g=2, b=1, stretch = "lin", main = "2026") # main = titolo
 
-# nir su red   
-plotRGB(pre2018, r=4, g=2, b=1, stretch = "lin", main = "2018")
-plotRGB(post2020, r=4, g=2, b=1, stretch = "lin", main = "2020")
+# NIR su Red
+
+plotRGB(pre2018, r=4, g=2, b=1, stretch = "lin", main = "2018")   # Metto banda B8 sul rosso
+plotRGB(post2020, r=4, g=2, b=1, stretch = "lin", main = "2020")  # Permette di mettere in evidenza la riflettanza della vegetazione
 plotRGB(post2026, r=4, g=2, b=1, stretch = "lin", main = "2026")
 
 #Elimino mare da DVI
-coast <- vect("AustraliaCosta.shp")
+
+coast <- vect("AustraliaCosta.shp") # importa shapefile come SpatVector
 
 pre2018 <- clipCoast(pre2018,coast)
 post2020 <- clipCoast(post2020,coast)
 post2026 <- clipCoast(post2026,coast)
 
-#DVI
-dvi_2018 <- im.dvi(pre2018, 4, 3)
-dvi_2020 <- im.dvi(post2020, 4, 3)
+#DVI Difference vegetation index
+
+dvi_2018 <- im.dvi(pre2018, 4, 3)    # Funzione di imageRy
+dvi_2020 <- im.dvi(post2020, 4, 3)   # Differenza di riflettanza tra banda NIR (4) e Rossa (3), mostra salute vegetazione
 dvi_2026 <- im.dvi(post2026, 4, 3)
 
 plot(dvi_2018, col = magma(100), main = "DVI 2018")
 plot(dvi_2020, col = magma(100), main = "DVI 2020")
 plot(dvi_2026, col = magma(100), main = "DVI 2026")
 
-#NDVI
-ndvi_2018 <- im.ndvi(pre2018, 4, 3)
+#N DVI Normalized difference vegetation index -> standardizzato per diverse risoluzioni radiometriche -> range -1 / +1
+
+ndvi_2018 <- im.ndvi(pre2018, 4, 3) # DVI(somma banda NRI e visibile (rossa)
 ndvi_2020 <- im.ndvi(post2020, 4, 3)
 ndvi_2026 <- im.ndvi(post2026, 4, 3)
 
@@ -83,17 +90,19 @@ plot(ndvi_2018, col = inferno(100), main = "NDVI 2018")
 plot(ndvi_2020, col = inferno(100), main = "NDVI 2020")
 plot(ndvi_2026, col = inferno(100), main = "NDVI 2026")
 
-#Stack
-stack_ndvi <- c(ndvi_2018, ndvi_2020, ndvi_2026) 
-names(stack_ndvi) <- c("NDVI 2018", "NDVI 2020", "NDVI 2026")
-im.ridgeline(stack_ndvi, scale = 1, palette = "plasma")
+#Analisi multitemporale della distribuzione dell'NDVI mediante ridgeline plot
 
-#NDVI diff
-dev.off()
+stack_ndvi <- c(ndvi_2018, ndvi_2020, ndvi_2026)               # Concatena più vettori
+names(stack_ndvi) <- c("NDVI 2018", "NDVI 2020", "NDVI 2026")  # Assegna nomi agli elementi dei vettori
+im.ridgeline(stack_ndvi, scale = 1, palette = "plasma") 
 
-ndvi_diff1 <- ndvi_2020 - ndvi_2018
-ndvi_diff2 <- ndvi_2026 - ndvi_2020
-ndvi_diff3 <- ndvi_2026 - ndvi_2018
+#Analisi temporale tramite differenze dell'NDVI
+
+dev.off() #chiude finestra grafica
+
+ndvi_diff1 <- ndvi_2020 - ndvi_2018  # Differenza tra prima dell'inizio e dopo la fine degli incendi
+ndvi_diff2 <- ndvi_2026 - ndvi_2020  # Differenza tra dopo la fine e il periodo di recupero
+ndvi_diff3 <- ndvi_2026 - ndvi_2018  # Differenza totale
 
 im.multiframe(1,3)
 
@@ -101,45 +110,57 @@ plot(ndvi_diff1, col = rocket(100), main = "2018 - 2020")
 plot(ndvi_diff2, col = rocket(100), main = "2020 - 2026")
 plot(ndvi_diff3, col = rocket(100), main = "2018 - 2026")
 
-#CLASSI
-class_2018 <- im.classify(ndvi_2018, seed = 96, num_cluster = 3)
-class_2020 <- im.classify(ndvi_2020, seed = 20, num_cluster = 3)
+# Classificazione non supervisionata delle tre immagini raster in 3 cluster
+
+class_2018 <- im.classify(ndvi_2018, seed = 96, num_cluster = 3) # R sceglie automaticamente i tipi di gruppi
+class_2020 <- im.classify(ndvi_2020, seed = 20, num_cluster = 3) # seed indica una delle iterazioni possibili
 class_2026 <- im.classify(ndvi_2026, seed = 09, num_cluster = 3)
+
+# Assegnazione delle etichette alle 3 classi
 
 levels(class_2018) <- data.frame(value = c(1, 2, 3), label = c("Mare", "Vegetazione", "Suolo nudo"))
 levels(class_2020) <- data.frame(value = c(1, 2, 3), label = c("Mare", "Vegetazione", "Suolo nudo"))
 levels(class_2026) <- data.frame(value = c(1, 2, 3), label = c("Mare", "Vegetazione", "Suolo nudo"))
 
-col_classes <- c("Mare" = "#ffffff","Vegetazione" = "#09622A", "Suolo nudo"="#E8C761")
+# Assegnazione colori personalizzati alle 3 classi
 
-#col_classes <- c("1" = "#ffffff","2" = "#09622A", "3"="#E8C761")
+col_classes <- c("Mare" = "#ffffff","Vegetazione" = "#09622A", "Suolo nudo"="#E8C761") # Mare colorato di bianco in quanto non d'interesse per le analisi
 
-plot(class_2018, col=col_classes, main="2018", legend=FALSE)
+# Visualizzazione delle classificazioni
+
+plot(class_2018, col=col_classes, main="2018", legend=FALSE) # legend=FALSE non mostra la legenda
 plot(class_2020, col=col_classes, main="2020", legend=FALSE)
 plot(class_2026, col=col_classes, main="2026", legend=FALSE)
 
-labels <- c("Vegetazione" = "#09622A", "Suolo nudo"="#E8C761")
+# Creazione Legenda
+
+labels <- c("Vegetazione" = "#09622A", "Suolo nudo"="#E8C761") # Colori per due cluster -> escluso Mare
 
 legend("bottomleft", 
        legend = names(labels), 
-       fill = labels, 
+       fill = labels,  # Determina colori in base a palette definita
        bg = "white",
-       xpd = TRUE)
+       xpd = TRUE)     # Può essere disegnata fuori dall'interfaccia grafica
 
-f_2018 <- freq(class_2018)
+# Calcolo delle frequenze
+
+f_2018 <- freq(class_2018) # Frequenza valori nel raster
 f_2020 <- freq(class_2020)
 f_2026 <- freq(class_2026)
 
-#Eliminiamo la classe del Mare
+# Eliminiamo la classe del Mare
 
-f_2018 <- f_2018[-1, ]
+f_2018 <- f_2018[-1, ] #Elimina tutti elementi della prima riga
 f_2020 <- f_2020[-1, ]
 f_2026 <- f_2026[-1, ]
 
-perc_2018 <- (f_2018$count / sum(f_2018[,3])) * 100
-perc_2020 <- (f_2020$count / sum(f_2018[,3])) * 100
+# Calcolo delle percentuali
+
+perc_2018 <- (f_2018$count / sum(f_2018[,3])) * 100  # Proporzione: Prende conteggi e divide per somma pixel terza colonna (Vegetazione + Suolo nudo)
+perc_2020 <- (f_2020$count / sum(f_2018[,3])) * 100  # Percentuale: Moltiplica prop per 100
 perc_2026 <- (f_2026$count / sum(f_2018[,3])) * 100
 
+# Creazione tabella riassuntiva
 
 tabella <- data.frame( 
   class = c("Vegetazione", "Suolo nudo"),
@@ -148,35 +169,38 @@ tabella <- data.frame(
   Post_2026 = perc_2026 ,
 )
 
+# Visualizzazione tabella
+
 tabella
 
 #        class  Pre_2018 Post_2020 Post_2026
 #1 Vegetazione 96.628875  63.62995 95.879389
 #2  Suolo Nudo  3.371125  36.29394  4.075564
 
-p1 <- ggplot(tabella, aes(x = class, y = perc_2018, fill = class)) +              #creazione del grafico usando il dataset tabella
-  geom_bar(stat = "identity") +                                                  #definizione del tipo di grafico (grafico a barre)                       
-  ylim(c(0, 100)) +                                                              #limiti asse y
-  scale_fill_manual(values = labels) +                                           #impostazione manuale dei colori delle barre
-  labs(title = "Copertura Pre incendio", x = "Classe", y = "Percentuale (%)") + #definizione etichette del grafico
-  theme(legend.position = "none")                                                #elimina la legenda del grafico
+# Generazione di istogrammi per il confronto
 
-p2 <- ggplot(tabella, aes(x = class, y = perc_2020, fill = class)) +              
+p1 <- ggplot(tabella, aes(x = class, y = perc_2018, fill = class)) +              # Creazione del grafico usando il dataset tabella
+  geom_bar(stat = "identity") +                                                   # Definizione del tipo di grafico (grafico a barre)                       
+  ylim(c(0, 100)) +                                                               # Limiti asse y
+  scale_fill_manual(values = labels) +                                            # Impostazione manuale dei colori delle barre
+  labs(title = "Copertura Pre incendio", x = "Classe", y = "Percentuale (%)") +   # Definizione etichette del grafico
+  theme(legend.position = "none")                                                 # Elimina la legenda del grafico
+
+p2 <- ggplot(tabella, aes(x = class, y = perc_2020, fill = class)) +              # Funzione di ggplot
   geom_bar(stat = "identity") +                                                                         
   ylim(c(0, 100)) +                                                              
   scale_fill_manual(values = labels) +                                           
   labs(title = "Copertura Post incendio (2020)", x = "Classe", y = "Percentuale (%)") + 
-  theme(legend.position = "none") 
+  theme(legend.position = "none")
 
-
-p3 <- ggplot(tabella, aes(x = class, y = perc_2026, fill = class)) +              
-  geom_bar(stat = "identity") +                                                                         
+p3 <- ggplot(tabella, aes(x = class, y = perc_2026, fill = class)) +              # aes definisce estetica: assi e colori che richiamano tabella
+  geom_bar(stat = "identity") +                                                   # assi x mostra le classi, y i valori percentuali         
   ylim(c(0, 100)) +                                                              
   scale_fill_manual(values = labels) +                                           
   labs(title = "Copertura Post incendio (2026)", x = "Classe", y = "Percentuale (%)") + 
   theme(legend.position = "none") 
 
-#visualizzazione dei grafici 
+# Visualizzazione dei grafici 
 
-p1 + p2 + p3
+p1 + p2 + p3 # Pacchetto patchwork permette di unire grafici
 
