b0993f8c574c2be94c476101319bef10e76008ad
MircoGruppi <mircogruppi.gruppi@gmail.com>
2026-09-03 16:53:34 +0200

    Update ESAME.r

--- ESAME/ESAME.r
+++ ESAME/ESAME.r
@@ -47,26 +47,29 @@ plot(post2026[[4]], col = rocket(100), main = "2026, B8")
 #visualizzazione a colori naturali
 im.multiframe(1,3)
 
-im.plotRGB(pre2018, r=3, g=2, b=1, title="2018")
-im.plotRGB(post2020, r=3, g=2, b=1, title="2020")
-im.plotRGB(post2026, r=3, g=2, b=1, title="2026")
+im.plotRGB(pre2018, r=3, g=2, b=1, title = "2018")
+im.plotRGB(post2020, r=3, g=2, b=1, title = "2020")
+im.plotRGB(post2026, r=3, g=2, b=1, title = "2026")
 
-# nir su red
-im.plotRGB(pre2018, r=4, g=2, b=1, title="2018")
-im.plotRGB(post2020, r=4, g=2, b=1, title="2020")
-im.plotRGB(post2026, r=4, g=2, b=1, title="2026")
+# nir su red   
+im.plotRGB(pre2018, r=4, g=2, b=1, title = "2018")
+im.plotRGB(post2020, r=4, g=2, b=1, title = "2020")
+im.plotRGB(post2026, r=4, g=2, b=1, title = "2026")
+#CAMBIA TITLE CON MAIN
+
 
 #Elimino mare da DVI   -> NON FUNGE
-coast <- vect("AustraliaCoast.shp")
+coast <- vect("AustraliaCosta.shp")
 
 pre2018 <- clipCoast(pre2018,coast)
-pre2020 <- clipCoast(pre2020,coast)
-pre2026 <- clipCoast(pre2026,coast)
+post2020 <- clipCoast(post2020,coast)
+post2026 <- clipCoast(post2026,coast)
 
 #DVI
 dvi_2018 <- im.dvi(pre2018, 4, 3)
 dvi_2020 <- im.dvi(post2020, 4, 3)
 dvi_2026 <- im.dvi(post2026, 4, 3)
+
 plot(dvi_2018, col = magma(100), main = "DVI 2018")
 plot(dvi_2020, col = magma(100), main = "DVI 2020")
 plot(dvi_2026, col = magma(100), main = "DVI 2026")
@@ -75,43 +78,66 @@ plot(dvi_2026, col = magma(100), main = "DVI 2026")
 ndvi_2018 <- im.ndvi(pre2018, 4, 3)
 ndvi_2020 <- im.ndvi(post2020, 4, 3)
 ndvi_2026 <- im.ndvi(post2026, 4, 3)
+
 plot(ndvi_2018, col = inferno(100), main = "NDVI 2018")
 plot(ndvi_2020, col = inferno(100), main = "NDVI 2020")
 plot(ndvi_2026, col = inferno(100), main = "NDVI 2026")
 
 #Stack
 stack_ndvi <- c(ndvi_2018, ndvi_2020, ndvi_2026) 
-names(stack_ndvi) <- c("NDVI 2016", "NDVI 2020", "NDVI 2026")
+names(stack_ndvi) <- c("NDVI 2018", "NDVI 2020", "NDVI 2026")
 im.ridgeline(stack_ndvi, scale = 1, palette = "plasma")
 
 #NDVI diff
 dev.off()
+
 ndvi_diff1 <- ndvi_2020 - ndvi_2018
 ndvi_diff2 <- ndvi_2026 - ndvi_2020
 ndvi_diff3 <- ndvi_2026 - ndvi_2018
+
 im.multiframe(1,3)
+
 plot(ndvi_diff1, col = rocket(100), main = "2018 - 2020")
 plot(ndvi_diff2, col = rocket(100), main = "2020 - 2026")
 plot(ndvi_diff3, col = rocket(100), main = "2018 - 2026")
 
 #CLASSI
-class_2018 <- im.classify(ndvi_2018, seed = 42, num_cluster = 4)
-class_2020 <- im.classify(ndvi_2020, seed = 42, num_cluster = 4)
-class_2026 <- im.classify(ndvi_2026, seed = 42, num_cluster = 4)
+class_2018 <- im.classify(ndvi_2018, seed = 96, num_cluster = 3)
+class_2020 <- im.classify(ndvi_2020, seed = 20, num_cluster = 3)
+class_2026 <- im.classify(ndvi_2026, seed = 09, num_cluster = 3)
 
-f_2016 <- freq(class_2016)
+levels(class_2018) <- data.frame(value = c(1, 2, 3), label = c("Mare", "Vegetazione", "Suolo nudo"))
+levels(class_2020) <- data.frame(value = c(1, 2, 3), label = c("Mare", "Vegetazione", "Suolo nudo"))
+levels(class_2026) <- data.frame(value = c(1, 2, 3), label = c("Mare", "Vegetazione", "Suolo nudo"))
+
+col_classes <- c("Mare" = "#ffffff","Vegetazione" = "#09622A", "Suolo nudo"="#E8C761")
+
+plot(class_2018, col=col_classes, main="2018", legend=FALSE)
+plot(class_2020, col=col_classes, main="2020", legend=FALSE)
+plot(class_2026, col=col_classes, main="2026", legend=FALSE)
+
+labels <- c("Vegetazione" = "#09622A", "Suolo nudo"="#E8C761")
+
+legend("bottomleft", 
+       legend = names(labels), 
+       fill = labels, 
+       bg = "white",
+       xpd = TRUE)
+
+f_2018 <- freq(class_2018)
+f_2020 <- freq(class_2020)
 f_2026 <- freq(class_2026)
 
-prop_2016 <- f_2016$count / ncell(class_2016)
-prop_2026 <- f_2026$count / ncell(class_2026)
+perc_2018 <- (f_2018$count / ncell(class_2018)) * 100
+perc_2020 <- (f_2020$count / ncell(class_2020)) * 100
+perc_2026 <- (f_2026$count / ncell(class_2026)) * 100
 
-perc_2016 <- prop_2016 * 100
-perc_2026 <- prop_2026 * 100
 
-tabella <- data.frame(
-  class = c(1, 2, 3),
-  P2016 = perc_2016,
-  P2025 = perc_2026
+tabella <- data.frame( 
+  class = c("Mare", "Vegetazione", "Suolo nudo"),
+  Pre_2018 = perc_2018 ,
+  Post_2020 = perc_2020 ,
+  Post_2026 = perc_2026 ,
 )
 
 tabella
